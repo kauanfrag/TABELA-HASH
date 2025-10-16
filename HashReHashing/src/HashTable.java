f94f7680bd70f8df358b97906b315a17d8fa8dc3
Lucas Maciel Cardoso <124613845+LucasC64@users.noreply.github.com>
2025-10-16 12:23:08 -0300

    Add files via upload

---
+++ HashReHashing/src/HashTable.java
@@ -0,0 +1,115 @@
+class HashTable {
+    private Registro[] tabela;
+    private int tamanho;
+    private int colisoes = 0;
+    private int numElementos = 0;
+
+    public HashTable(int tamanho) {
+        this.tamanho = tamanho;
+        this.tabela = new Registro[tamanho];
+    }
+
+    public int getColisoes() {
+        return colisoes;
+    }
+
+    int hashQuadratico(int chave, int i) {
+        int h = chave % tamanho;
+        return (h + 1 * i + 3 * i * i) % tamanho;
+    }
+
+    public void inserir(int chave) {
+        if ((float) numElementos / tamanho >= 0.7) {
+            rehashExpandir();
+        }
+
+        int i = 0;
+        int pos;
+
+        while (i < tamanho) {
+            pos = hashQuadratico(chave, i);
+
+            if (tabela[pos] == null) {
+                tabela[pos] = new Registro(chave);
+                numElementos++;
+                System.out.println("* Inserido " + chave + " na posição " + pos);
+                return;
+            } else {
+                colisoes++;
+            }
+            i++;
+        }
+
+    }
+
+    public boolean buscar(int chave) {
+        int i = 0;
+        int pos;
+
+        while (i < tamanho) {
+            pos = hashQuadratico(chave, i);
+
+            if (tabela[pos] == null){
+                return false;
+            }
+            if (tabela[pos].chave == chave){
+                return true;
+            }
+
+            i++;
+        }
+
+        return false;
+    }
+
+    private void rehashExpandir() {
+        Registro[] antiga = tabela;
+        tamanho = tamanho * 2;
+        tabela = new Registro[tamanho];
+        colisoes = 0;
+        numElementos = 0;
+
+        for (Registro r : antiga) {
+            if (r != null) {
+                inserir(r.chave);
+            }
+        }
+    }
+
+    public int[] calcularGaps() {
+        int menorGap = 1000000000;
+        int maiorGap = 0;
+        int somaGaps = 0;
+        int quantidadeGaps = 0;
+        int indiceUltimoOcupado = -1;
+
+        for (int i = 0; i < tabela.length; i++) {
+            if (tabela[i] != null) {
+                if (indiceUltimoOcupado != -1) {
+                    int gap = i - indiceUltimoOcupado - 1;
+                    if (gap < menorGap) {
+                        menorGap = gap;
+                    }
+                    if (gap > maiorGap) {
+                        maiorGap = gap;
+                    }
+                    somaGaps += gap;
+                    quantidadeGaps++;
+                }
+                indiceUltimoOcupado = i;
+            }
+        }
+
+        int mediaGap;
+        if (quantidadeGaps == 0) {
+            mediaGap = 0;
+        } else {
+            mediaGap = somaGaps / quantidadeGaps;
+        }
+
+        int[] gaps = new int[]{menorGap, maiorGap, mediaGap};
+
+        return gaps;
+    }
+
+}
